c9e3f3b5c1991e95e95d13963308236ed532160a
Julie <J-Buma@hotmail.com>
2018-01-14 15:16:22 +0100

    Preloading pages

--- public/js/main.js
+++ public/js/main.js
@@ -2,22 +2,38 @@ var contactBtn = document.getElementById('contact-btn');
 var portfolioBtn = document.getElementById('portfolio-btn');
 var contentBox = document.getElementById('content-box');
 
+var contactPage = '';
+var homePage = '';
+var portfolioPage = '';
+
+$(document).ready(()=>{
+	$.get('public/views/contact.html',function(data){
+		contactPage = data;
+	});
+	$.get('public/views/home.html',function(data){
+		homePage = data;
+	});
+	$.get('public/views/portfolio.html',function(data){
+		portfolioPage = data;
+	});
+})
+
 $('body').on('click', '#contact-btn', function() {
-	loadPage('contact');
+	loadPage(contactPage);
 });
 
 $('body').on('click','#home-btn',function() {
-	loadPage('home');
+	loadPage(homePage);
 });
 
 $('body').on('click','#portfolio-btn',function() {
-	loadPage('portfolio');
+	loadPage(portfolioPage);
 });
 
 function loadPage(page){
 	contentBox.classList.add('new-page');
 	setTimeout(function(){
-		$('#content-box').load('public/views/'+page+'.html');
+		$('#content-box').html(page);
 
 		setTimeout(function(){
 			contentBox.classList.remove('new-page');
